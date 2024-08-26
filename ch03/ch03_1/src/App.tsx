ea5a2d4c9057d79e934005ad00384cfc0d19c44d
ohdoyoel <ohdoyoel@naver.com>
2024-08-26 17:53:32 +0900

    init

---
+++ ch03/ch03_1/src/App.tsx
@@ -0,0 +1,18 @@
+import Bootstrap from './pages/Bootstrap'
+import Icon from './pages/Icon'
+import UsingIcon from './pages/UsingIcon'
+import UsingIconWithCSSClass from './pages/UsingIconWithCSSClass'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <UsingIconWithCSSClass />
+      <UsingIcon />
+      <Icon />
+      {/* <Bootstrap /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,9 @@
+import Color from './pages/Color'
+import Tailwindcss from './pages/Tailwindcss'
+import TextsTest from './pages/TextsTest'
+
+function App() {
+  return <TextsTest />
+}
+
+export default App
